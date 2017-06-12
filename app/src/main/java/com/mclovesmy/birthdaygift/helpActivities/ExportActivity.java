2a6db834f9f2645fbd7de52a28afb7c38c008adf
Daniël van Dijken <vandijkendaniel@hotmail.com>
2017-06-12 20:37:41 +0200

    Create settings activities and xml files. Make sure headers are skipped, jump right into pref_general.

---
+++ app/src/main/java/com/mclovesmy/birthdaygift/helpActivities/ExportActivity.java
@@ -0,0 +1,89 @@
+package com.mclovesmy.birthdaygift.helpActivities;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.mclovesmy.birthdaygift.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ExportActivity extends AppCompatActivity {
+
+    JSONArray SQLContents;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export);
+
+        SQLContents = getSQLContents();
+
+        EditText exportEditText = (EditText) findViewById(R.id.exportEditText);
+
+        //Clear edittext and set new SQLContents
+        exportEditText.getText().clear();
+        exportEditText.setText("" + SQLContents);
+    }
+
+    public void copy () {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(ClipData.newPlainText("", String.valueOf(SQLContents)));
+    }
+
+    private JSONArray getSQLContents() {
+
+        String myPath = String.valueOf(getDatabasePath("BIRTHDAYS.DB"));//Path to database
+
+        String myTable = "BIRTHDAYS";//Table name
+
+        SQLiteDatabase myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        String searchQuery = "SELECT  * FROM " + myTable;
+        Cursor cursor = myDataBase.rawQuery(searchQuery, null );
+
+        JSONArray resultSet = new JSONArray();
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+
+            int totalColumn = cursor.getColumnCount();
+            JSONObject rowObject = new JSONObject();
+
+            for( int i=0 ;  i< totalColumn ; i++ )
+            {
+                if( cursor.getColumnName(i) != null )
+                {
+                    try
+                    {
+                        if( cursor.getString(i) != null )
+                        {
+                            Log.d("TAG_NAME", cursor.getString(i) );
+                            rowObject.put(cursor.getColumnName(i) ,  cursor.getString(i) );
+                        }
+                        else
+                        {
+                            rowObject.put( cursor.getColumnName(i) ,  "" );
+                        }
+                    }
+                    catch( Exception e )
+                    {
+                        Log.d("TAG_NAME", e.getMessage()  );
+                    }
+                }
+            }
+            resultSet.put(rowObject);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        Log.d("TAG_NAME", resultSet.toString() );
+        return resultSet;
+    }
+}
---
+++ app/src/main/java/com/mclovesmy/birthdaygift/helpActivities/ImportActivity.java
@@ -0,0 +1,13 @@
+package com.mclovesmy.birthdaygift.helpActivities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ImportActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
